80e4d090427e0d552e6347af8b0db43b642b90f0
Kazunori OYA <terje@oya3.net>
2019-11-10 22:23:54 +0900

    re-create ObservableCollection

--- WpfApp1/Models/UsersModel.cs
+++ WpfApp1/Models/UsersModel.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace WpfApp1.Models
 {
+    /*
+    public static void Sort<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+    {
+        var sortableList = new List<T>(collection);
+        sortableList.Sort(comparison);
+
+        for (int i = 0; i < sortableList.Count; i++)
+        {
+            collection.Move(collection.IndexOf(sortableList[i]), i);
+        }
+    }
+    */
+
     class UsersModel //: Common.BindableBase
     {
         private static readonly UsersModel instance = new UsersModel();
         public ObservableCollection<UserModel> Users;
+        public bool StopWorker { get; set; } = false;
+
         private UsersModel()
         {
             this.Users = new ObservableCollection<UserModel>();
-            for (int index = 0; index <= 100; index++)
+            for (int index = 0; index < 100; index++)
             {
                 var user = new UserModel();
                 user.ID = index;
@@ -20,6 +39,30 @@ namespace WpfApp1.Models
                 user.Address = string.Format("{0:D3}", index) + "address";
                 user.Tel = string.Format("{0:D2}", index) + "-000-0000";
                 this.Users.Add(user);
+            }
+
+            // Task.Run(() => this.Worker());
+        }
+
+        private void Worker()
+        {
+            int mode = 0; // 0,1:順番入れ替え、1:削除、2:追加
+            while (false == this.StopWorker)
+            {
+                Thread.Sleep(2000);
+                if (0 == mode)
+                {
+                    //this.Users.Sort((a, b) => { return a.CompareTo(b); });
+                    // this.Users = new ObservableCollection<UserModel>(this.Users.OrderByDescending(n => n.ID));
+                    mode++;
+                }
+                else if (1 == mode)
+                {
+
+                    // this.Users = new ObservableCollection<UserModel>(this.Users.OrderBy(n => n.ID));
+                    mode++;
+                    mode = 0;
+                }
             }
         }
 
--- WpfApp1/ViewModels/UsersViewModel.cs
+++ WpfApp1/ViewModels/UsersViewModel.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Windows.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace WpfApp1.ViewModels
 {
-    class UsersViewModel : /*Common.BindableBase,*/ IPageViewModel
+    class UsersViewModel : Common.BindableBase, IPageViewModel
     {
         // public ObservableCollection<Models.UserModel> Users { get; set; } = Models.UsersModel.Instance.Users;
         public ObservableCollection<Models.UserModel> Users { get; set; }
+        public CollectionViewSource UsersViewSource { get; private set; }
+        public bool StopWorker { get; set; } = false;
+        private SynchronizationContext _mainContext;
 
         public UsersViewModel(Models.UserModel user = null)
         {
             this.Users = Models.UsersModel.Instance.Users;
+            this.UsersViewSource = new CollectionViewSource()
+            {
+                Source = this.Users
+            };
+            this.UserIDSort(ListSortDirection.Descending);
             // - ObservableCollectionの中身が変更されたことを検知する方法
             //   https://aroundthedistance.hatenadiary.jp/entry/2014/07/25/200417
             // this.Users.CollectionChanged += Users_CollectionChanged;
@@ -23,8 +36,61 @@ namespace WpfApp1.ViewModels
             {
                 SelectedUser = user;
             }
+            _mainContext = SynchronizationContext.Current;
+            Task.Run(() => this.Worker());
+        }
+
+        public void Worker()
+        {
+            int mode = 0; // 0,1:順番入れ替え、1:削除、2:追加
+            while (false == this.StopWorker)
+            {
+                Thread.Sleep(2000);
+                if (0 == mode)
+                {
+                    //this.Users.Sort((a, b) => { return a.CompareTo(b); });
+                    _mainContext.Post(_ =>
+                    {
+                        this.Users = new ObservableCollection<Models.UserModel>(Models.UsersModel.Instance.Users.OrderByDescending(n => n.ID));
+                        RaisePropertyChanged("Users");
+                    }, null);
+                    mode++;
+                }
+                else if (1 == mode)
+                {
+                    // this.Users = new ObservableCollection<UserModel>(this.Users.OrderBy(n => n.ID));
+                    _mainContext.Post(_ =>
+                    {
+                        this.Users = new ObservableCollection<Models.UserModel>(Models.UsersModel.Instance.Users.OrderBy(n => n.ID));
+                        RaisePropertyChanged("Users");
+                    }, null);
+                    mode++;
+                    mode = 0;
+                }
+                /*
+                if (0 == mode)
+                {
+                    _mainContext.Post(_ => this.UserIDSort(ListSortDirection.Ascending), null);
+                    mode++;
+                }
+                else if (1 == mode)
+                {
+                    _mainContext.Post(_ => this.UserIDSort(ListSortDirection.Descending), null);
+                    mode++;
+                    mode = 0;
+                }
+                */
+            }
 
         }
+
+        public void UserIDSort(ListSortDirection direction)
+        {
+            UsersViewSource.SortDescriptions.Clear();
+            UsersViewSource.SortDescriptions.Add(new SortDescription("ID", ListSortDirection.Ascending));
+            //aisePropertyChanged("UsersViewSource");
+        }
+
         /* 
         private void Users_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
